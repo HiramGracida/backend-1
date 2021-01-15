fa9a9f886dd26d93a6a476ce56dc97b774364035
HiramGracida <neocrofts@icloud.com>
2021-01-15 06:16:29 -0600

    Avance de todos los videos

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("Hola Mundo!");
+const express = require('express');
+require('dotenv').config();
+
+//Crear servidor express
+const app = express();
+
+// Acceso al directorio publico
+app.use(express.static('public'));
+
+//rutas
+app.use('/api/auth', require('./routes/auth'));
+
+//Configurarción del puerto
+app.listen(process.env.PORT, () => {
+    console.log(`Backend corriendo en el puerto ${process.env.PORT}`);
+})
